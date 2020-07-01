4bdd967332d358f41d232c0b5bcc416b195c65bb
Quinn McPhail <quinnmcphail@me.com>
2020-06-30 21:41:45 -0400

    Linter Changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,7 +11,7 @@ exports.createPages = ({ graphql, actions }) => {
     const blogPost = path.resolve('./src/templates/blog-post.js');
 
     // Create index pages for all supported languages
-    Object.keys(supportedLanguages).forEach(langKey => {
+    Object.keys(supportedLanguages).forEach((langKey) => {
       createPage({
         path: langKey === 'en' ? '/' : `/${langKey}/`,
         component: path.resolve('./src/templates/blog-index.js'),
@@ -44,7 +44,7 @@ exports.createPages = ({ graphql, actions }) => {
             }
           }
         `
-      ).then(result => {
+      ).then((result) => {
         if (result.errors) {
           console.log(result.errors);
           reject(result.errors);
@@ -108,7 +108,7 @@ exports.createPages = ({ graphql, actions }) => {
           const otherLangPosts = posts.filter(
             ({ node }) => node.fields.langKey !== 'en'
           );
-          _.each(otherLangPosts, post => {
+          _.each(otherLangPosts, (post) => {
             const translations =
               translationsByDirectory[_.get(post, 'node.fields.directoryName')];
 
